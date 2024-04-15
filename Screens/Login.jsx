de9fff29db86e56b2d59656a10f7b9cb5c85b6f2
karandevelops <karankumar560k@gmail.com>
2024-04-16 02:54:24 +0530

    saved integration  changes

--- Screens/Login.jsx
+++ Screens/Login.jsx
@@ -1,10 +1,16 @@
-
-import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button, Alert } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
 import axios from 'axios';
-import { apiConnector } from '../Services/ApiConnecter';
-import {updateUser} from '../Redux/Slices/UserSlice'
-import { endpoints } from '../Services/api';
+import {apiConnector} from '../Services/ApiConnecter';
+import {updateUser} from '../Redux/Slices/UserSlice';
+import {endpoints} from '../Services/api';
 import {useSelector, useDispatch} from 'react-redux';
 import {Picker} from '@react-native-picker/picker';
 
@@ -15,16 +21,18 @@ const Login = ({navigation}) => {
   const [password, setPassword] = useState('');
   const handleLogin = async () => {
     try {
-      const response = await apiConnector(`POST`,endpoints.LOGIN_API,{email:name,password});
+      const response = await apiConnector(`POST`, endpoints.LOGIN_API, {
+        email: name,
+        password,
+      });
       // Handle success
       const user = response?.data?.data;
-      dispatch(updateUser(response?.data?.data))
-      console.log("the user is ", user)
-      if(user?.type === 'user'){
-        navigation.navigate('userHome')
-      }
-      else{
-        navigation.navigate('ads')
+      dispatch(updateUser(response?.data?.data));
+      console.log('the user is ', user);
+      if (user?.type === 'user') {
+        navigation.navigate('userHome');
+      } else {
+        navigation.navigate('ads');
       }
     } catch (error) {
       // Handle error
@@ -33,41 +41,62 @@ const Login = ({navigation}) => {
   };
 
   return (
-    <View>
-      <Text>Login</Text>
-      <TextInput
-        placeholder="Name"
-        value={name}
-        onChangeText={setName}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Login" onPress={handleLogin} />
-      <Button title="Create new Account" onPress={()=>{
-        navigation.navigate('signup')
-      }} />
+    <View className="items-center justify-center flex-1 bg-bgLessDark">
+      <Text className="text-textLight font-[600] w-[90%] py-4  text-center ml-2 text-2xl mt-8 capitalize pl-3">
+        Login Page
+      </Text>
+
+      <View className="w-[90%] mx-auto flex items-center mt-4">
+        <Text className="w-[100%] px-4 mb-0 text-textLight font-bold opacity-9 ">
+          Enter Your Mail Below:
+        </Text>
+        <TextInput
+          value={name}
+          onChangeText={setName}
+          className="px-4 placeholder:text-slate-400 mt-0 py-1 w-[100%] mb-6 capitalize border-b-2 rounded-lg border-b-white focus:border-bgPrimary border-opacity-5 opacity-70 text-textLight"
+        />
+        <Text className="w-[100%] px-4 mb-0 text-textLight font-bold opacity-9 ">
+          Enter Your Password Below:
+        </Text>
+        <TextInput
+          // placeholder="Pa"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          className="px-4 placeholder:text-slate-400 mt-0 py-1 w-[100%] mb-6 capitalize border-b-2 rounded-lg border-b-white focus:border-bgPrimary border-opacity-5 opacity-70 text-textLight"
+        />
+      </View>
+      <TouchableOpacity
+        className="w-[90%] mx-auto mt-6 mb-0 rounded-full py-4 pb-2"
+        onPress={handleLogin}>
+        <Text className="text-bgdark w-[100%] text-center bg-[#f2894dc7] p-4 rounded-lg">
+          Sign In
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        className="w-[90%] mx-auto mt-0 rounded-full py-2"
+        onPress={() => {
+          navigation.navigate('signup');
+        }}>
+        <Text className="text-bgdark w-[100%] text-center bg-[#f2894dc7] p-4 rounded-lg">
+          Create a new Account
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-
-const Signup = ({ navigation }) => {
-  const {info}= useSelector((state)=>state.user)
+const Signup = ({navigation}) => {
+  const {info} = useSelector(state => state.user);
   useEffect(() => {
-    if(info)
-    {
-      if(info.type === 'user'){
-        navigation.navigate('userHome')
-       }
-       else{
-        navigation.navigate('ads')
-       }
+    if (info) {
+      if (info.type === 'user') {
+        navigation.navigate('userHome');
+      } else {
+        navigation.navigate('ads');
       }
-  },[])
+    }
+  }, []);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -75,12 +104,13 @@ const Signup = ({ navigation }) => {
   const [verified, setVerified] = useState(false);
   const [accountType, setAccountType] = useState('user'); // Default account type is user
 
-
   const handleVerifyEmail = async () => {
     try {
       // Call your API to send OTP to the email
       // console.log("the email is ", email,endpoints.SENDOTP_API)
-      const response = await apiConnector('POST', endpoints.SENDOTP_API, { email });
+      const response = await apiConnector('POST', endpoints.SENDOTP_API, {
+        email,
+      });
       setVerified(true);
       Alert.alert('Verification email sent!');
     } catch (error) {
@@ -89,9 +119,15 @@ const Signup = ({ navigation }) => {
   };
   const handleSignup = async () => {
     try {
-      const response = await apiConnector("POST",endpoints.SIGNUP_API,{name,email,password,otp,accountType});
+      const response = await apiConnector('POST', endpoints.SIGNUP_API, {
+        name,
+        email,
+        password,
+        otp,
+        accountType,
+      });
       // console.log(response.data);
-      if(response.data.success){
+      if (response.data.success) {
         Alert.alert('Signup successful!');
         navigation.navigate('login');
       }
@@ -102,45 +138,83 @@ const Signup = ({ navigation }) => {
   };
 
   return (
-    <View>
-      <Text>Signup</Text>
+    <View className="items-center justify-center flex-1 bg-bgLessDark">
+      <Text className="text-textLight font-[600] w-[90%] py-4  text-center ml-2 text-2xl mt-8 capitalize pl-3">
+        Sign Up Page
+      </Text>
       {!verified ? (
         <>
-          <TextInput
-            placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
-            keyboardType="email-address"
-          />
-          <Button title="Verify Email" onPress={handleVerifyEmail} />
+          <View className="w-[90%] mx-auto flex items-center mt-4">
+            <Text className="w-[100%] px-4 mb-0 text-textLight font-bold opacity-9 ">
+              Enter Your Mail Below:
+            </Text>
+
+            <TextInput
+              placeholder="Email"
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+              className="px-4 placeholder:text-slate-400 mt-0 py-1 w-[100%] mb-6 capitalize border-b-2 rounded-lg border-b-white focus:border-bgPrimary border-opacity-5 opacity-70 text-textLight"
+            />
+            <TouchableOpacity
+              className="w-[90%] mx-auto mt-0 rounded-full py-2"
+              onPress={handleVerifyEmail}>
+              <Text className="text-bgdark w-[100%] text-center bg-[#f2894dc7] p-4 rounded-lg">
+                Verify Email
+              </Text>
+            </TouchableOpacity>
+          </View>
         </>
       ) : (
-        <>
+        <View className="w-[90%] mx-auto flex items-center mt-4">
+          <Text className="w-[100%] px-4 mb-0 text-textLight font-bold opacity-9 ">
+            Enter Your Name Below:
+          </Text>
           <TextInput
             placeholder="Name"
             value={name}
             onChangeText={setName}
+            className="px-4 placeholder:text-slate-400 mt-0 py-1 w-[100%] mb-6 capitalize border-b-2 rounded-lg border-b-white focus:border-bgPrimary border-opacity-5 opacity-70 text-textLight"
           />
+          <Text className="w-[100%] px-4 mb-0 text-textLight font-bold opacity-9 ">
+            Enter Your Password Below:
+          </Text>
           <TextInput
             placeholder="Password"
             value={password}
             onChangeText={setPassword}
             secureTextEntry
+            className="px-4 placeholder:text-slate-400 mt-0 py-1 w-[100%] mb-6 capitalize border-b-2 rounded-lg border-b-white focus:border-bgPrimary border-opacity-5 opacity-70 text-textLight"
           />
+          <Text className="w-[100%] px-4 mb-0 text-textLight font-bold opacity-9 ">
+            Enter the recieved OTP Below:
+          </Text>
           <TextInput
             placeholder="OTP"
             value={otp}
             onChangeText={setOtp}
+            className="px-4 placeholder:text-slate-400 mt-0 py-1 w-[100%] mb-6 capitalize border-b-2 rounded-lg border-b-white focus:border-bgPrimary border-opacity-5 opacity-70 text-textLight"
           />
+          <Text className='w-[100%] px-4 mb-0 text-textLight font-bold opacity-9 '>Choose the AccountType:</Text>
+       <View className='border-b-2 border-b-bgPrimary mb-4 w-[100%] font-bold'> 
           <Picker
             selectedValue={accountType}
-            onValueChange={(itemValue) => setAccountType(itemValue)}
-          >
+            themeVariant={"light"}
+            onValueChange={itemValue => setAccountType(itemValue)}
+            style={{backgroundColor: 'transperent', color: '#fff', border:'2px solid red'}}
+            itemStyle={{backgroundColor: '#25252C', color: '#F1F2EC'}}>
             <Picker.Item label="User" value="user" />
             <Picker.Item label="Advertiser" value="advertiser" />
           </Picker>
-          <Button title="Signup" onPress={handleSignup} />
-        </>
+          </View>
+          <TouchableOpacity
+            className="w-[90%] mx-auto mt-0 rounded-full py-2"
+            onPress={handleSignup}>
+            <Text className="text-bgdark w-[100%] text-center bg-[#f2894dc7] p-4 rounded-lg">
+              Create New Account
+            </Text>
+          </TouchableOpacity>
+        </View>
       )}
     </View>
   );
@@ -148,5 +222,4 @@ const Signup = ({ navigation }) => {
 
 export default Signup;
 
-
-export { Login, Signup };
+export {Login, Signup};
